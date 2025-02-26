1aef4cbb06f46e17b1e34d65834d8245e0811868
Volodymyr Vintila <vovavintila@gmail.com>
2025-02-26 10:58:57 +0100

    Meeting app implementation

--- meeting_room/settings.py
+++ meeting_room/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'meeting-room.vintila.dev',
+    'localhost',
+]
 
 
 # Application definition
